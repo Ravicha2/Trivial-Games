a9a90157b88f9c715b06ed97d4ec9aa2844c982d
Ravicha <rsuksawasdi@gmail.com>
2026-04-24 16:11:39 +1000

    ref callback tetris

--- src/components/Game10.jsx
+++ src/components/Game10.jsx
@@ -2,16 +2,22 @@ import { useState, useEffect, useRef } from "react"
 
 const randomShape = () => {
     const shapes = [
-        [[1,0,0],
-        [1,1,1],],
-        [[0,1,0],
-        [1,1,1]],
-        [[1,1,1,1]]
+        [
+            [1,0,0],
+            [1,1,1],
+        ],
+        [
+            [0,1,0],
+            [1,1,1]
+        ],
+        [
+            [1,1,1,1]
+        ]
     ]
     return shapes[Math.floor(Math.random() * shapes.length)]
 }
 
-const createBoard = () => {
+const createEmptyBoard = () => {
     let board = []
     for (let i=0; i<20;i++) {
         let row = []
@@ -24,74 +30,171 @@ const createBoard = () => {
 }
 
 const Game10 = () => {
-    const [board, setBoard] = useState(createBoard())
+    const [lockedBoard, setLockedBoard] = useState(createEmptyBoard())
     const [currentShape, setCurrentShape] = useState(randomShape())
-    const shapeRef = useRef( {row:0, col:0} );
+    const [shapePos, setShapePos] = useState({ row: 0, col: 0 });
+
+    const validMove = (targetRow, targetCol) => {
+        for (let r=0; r < currentShape.length; r++) {
+            for (let c=0; c < currentShape[r].length; c++) {
+                const isSolidBlock = currentShape[r][c] !== 0
+
+                if (isSolidBlock) {
+                    const predictedRow = targetRow + r
+                    const predictedCol = targetCol + c
+
+                    // hit border
+                    if (predictedRow >= 20 || predictedCol >= 10 || predictedCol < 0) return false
+                    // hit cement
+                    if (lockedBoard[predictedRow][predictedCol] !== 0) return false
+
+                }
+            }
+        }
+        return true
+    }
+
+    const attemptMoveDown = () => {
+        const nextRow = shapePos.row + 1
+        if (validMove(nextRow, shapePos.col)) {
+            setShapePos({...shapePos, row: nextRow})
+        } else {
+            cementShape();
+            spawnNewShape();
+        }
+    }
+
+    const attemptMoveX = (direction) => {
+        const nextCol = shapePos.col + direction
+        const end = currentShape.length + nextCol
+        if (nextCol < 0 || end >= 10) return
+        if (validMove(shapePos.row, nextCol)) {
+            shapePos.col = nextCol;
+        }
+    }    
+
+    const cementShape = () => {
+        let newLockedBoard = lockedBoard.map(row => [...row])
+        for (let r=0; r < currentShape.length; r++) {
+            for (let c=0; c < currentShape[r].length; c++) {
+                // if block, move down and add to cement
+                if (currentShape[r][c] !== 0){
+                    const finalRow = shapePos.row + r
+                    const finalCol = shapePos.col + c
+                    newLockedBoard[finalRow][finalCol] = currentShape[r][c]
+                }
+            } 
+        }
+        setLockedBoard(newLockedBoard);
+    }
+
+    const spawnNewShape = () => {
+        setCurrentShape(randomShape())
+        setShapePos({ row:0, col:0})
+    }
+
+    const getDisplayBoard = () => {
+        let displayBoard = lockedBoard.map(row => [...row])
+        for (let r=0; r < currentShape.length; r++) {
+            for (let c=0; c < currentShape[r].length; c++) {
+                if (currentShape[r][c] !== 0) {
+                    const drawRow = shapePos.row + r
+                    const drawCol = shapePos.col + c
+
+                    if (drawRow < 20 && drawCol >=0 && drawCol < 20) {
+                        displayBoard[drawRow][drawCol] = currentShape[r][c]
+                    }
+                }
+            }
+        }
+        return displayBoard
+    }
 
     const renderBoard = (board) => {
-        return (
-            board.map((row, rowIdx) => {
-                return (
-                    <div 
-                        key={rowIdx}
-                        className="flex flex-row justify-center "
-                    >
-                        {row.map((col, colIdx) => {
-                            return (
-                                <div 
-                                    key={colIdx}
-                                    className={`border w-6 h-4 text-xs ${col === 0 ? 'bg-transparent' : 'bg-blue-400'}`}
-                                >
-                                </div>
-                            )
-                        })}
-                    </div>
-                )
+        return (board.map((row, rowId) => {
+                return (<div 
+                    key={rowId}
+                    className="flex flex-row w-fit">
+                    {row.map((col, colId) => {
+                        return (
+                        <div
+                            className={`flex flex-row w-5 h-5 ${col === 0 ? 'bg-transparent' : (lockedBoard[rowId][colId] === 1 ? 'bg-green-500' :'bg-blue-500')}`}
+                            key={colId}
+                        >
+                        </div>)
+                    })}
+                </div>)
             })
         )
     }
 
-    const moveShape = (board, currentShapeRef, shape,moveRow, moveCol) => {
-        let newBoard = board.map(row => [...row])
-        let newShapeCoord = []
-        let oldShapeCoord = []
-        const currentRow = parseInt(currentShapeRef.current.row)
-        const currentCol = parseInt(currentShapeRef.current.col)
-        for (let [rowIdx, row] of shape.entries()) {
-            let newRowCoord = []
-            let oldRowCoord = []
-            for (let [colIdx, col] in row) {
-                const rowInt = parseInt(rowIdx)
-                const colInt = parseInt(colIdx)
-                newRowCoord.push([currentRow+rowInt+moveRow, currentCol+colInt+moveCol])
-                oldRowCoord.push([currentRow+rowInt, currentCol+colInt])
+    const savedAttemptMoveDown = useRef();
+
+    useEffect(() => {
+        savedAttemptMoveDown.current = attemptMoveDown
+    })
+
+    const savedHandleSwipe = useRef();
+
+    useEffect(() => {
+        savedHandleSwipe.current = (e) => {
+            e.preventDefault();
+            if (e.key === "ArrowLeft") {
+                attemptMoveX(-1);
+            } else if (e.key === "ArrowRight") {
+                attemptMoveX(1);
             }
-            newShapeCoord.push(newRowCoord)
-            oldShapeCoord.push(oldRowCoord)
-        }
-        for (let [rowIdx, row] of oldShapeCoord.entries()) {
-            for (let [colIdx, col] of row.entries()) {
-                if (col[0] < 0 || col[0] > 9 || col[1] < 0 || col[1] > 9) return;
-                newBoard[col[0]][col[1]] = 0
+        };
+    });
+
+    useEffect(() => {
+        const listener = (e) => {
+            if (savedHandleSwipe.current) {
+                savedHandleSwipe.current(e);
+            }
+        };
+    
+        window.addEventListener("keydown", listener);
+        
+        return () => {
+            window.removeEventListener("keydown", listener);
+        };
+    }, []);
+
+    useEffect(() => {
+        const tickRate = 750
+        const gameLoop = setInterval(() => {
+            if (savedAttemptMoveDown.current) {
+                savedAttemptMoveDown.current()
+            }
+        }, tickRate)
+        return () => clearInterval(gameLoop);
+    }, [])
+
+    useEffect(() => {
+        const handleSwipe = (e) => {
+            e.preventDefault()
+            if (e.key === "ArrowLeft") {
+                attemptMoveX(-1)
+            } else if (e.key === "ArrowRight") {
+                attemptMoveX(1)
+            } else {
+                return
             }
         }
-        for (let [rowIdx, row] of newShapeCoord.entries()) {
-            for (let [colIdx, col] of row.entries()) {
-                if (col[0] < 0 || col[0] > 9 || col[1] < 0 || col[1] > 9) return;
-                newBoard[col[0]][col[1]] = shape[rowIdx][colIdx]
-            }
+        
+        window.addEventListener("keydown", handleSwipe)
+        return () => {
+            window.removeEventListener("keydown", handleSwipe)
         }
-        setBoard(newBoard)
-        shapeRef.current = {row: shapeRef.current.row + moveRow, col: shapeRef.current.col + moveCol }
-    }
+    })
 
     return (
         <>
             <h1>Hello lets play the Tetris!</h1>
-            <div>
-                {renderBoard(board)}
+            <div className="border w-fit mx-auto">
+                {renderBoard(getDisplayBoard())}
             </div>
-            <button onClick={() => moveShape(board, shapeRef, currentShape, 1, 1)}>move</button>
         </>
     )
 }
